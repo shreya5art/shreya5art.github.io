be54e7e9302c79103945c1fd39a59fbe2875538e
shreya5art <124759466+shreya5art@users.noreply.github.com>
2023-03-15 19:02:11 +0530

    update 3

--- files/js/common.js
+++ files/js/common.js
@@ -1,3 +1,31 @@
+var icons = {
+    search: "fa-solid fa-magnifying-glass",
+    cart: "icon-shopping-cart",
+    star_fill: "icon-star-full",
+    star_half: "icon-star-half",
+    star_empty: "icon-star-empty",
+
+    menu: "icon-bars",
+
+    sun: "icon-cloud-sun-rain",
+    moon: "icon-cloud-moon",
+
+    filter: "icon-filter",
+    sort: "icon-sort-amount-asc",
+
+    facebook: "icon-facebook-square",
+    instagram: "icon-instagram",
+    twitter: "icon-twitter",
+    youtube: "icon-youtube-play",
+    telegram: "icon-telegram",
+
+    paypal: "icon-paypal",
+    upi: { code: '<span class="icon-UPI-Color"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span><span class="path20"></span><span class="path21"></span><span class="path22"></span><span class="path23"></span><span class="path24"></span><span class="path25"></span><span class="path26"></span><span class="path27"></span><span class="path28"></span><span class="path29"></span></span>' },
+    creditCard: "icon-credit-card",
+    wallet: "icon-wallet",
+
+}
+
 var customBasePrice = 1500;
 
 var sizes = {
